2f605e4229f3681ed4f07f3f6c0ec8d9ebd1d45c
GharrowBM <gharrow.games@gmail.com>
2021-11-18 12:41:15 +0100

    BankApp Basics

---
+++ FormationHarshaVardhan/BankApp/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace BankApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***BankApp***");
+            Console.WriteLine("::Login Page::");
+
+            string userName = null, password = null;
+
+            Console.Write("Username: ");
+            userName = Console.ReadLine();
+
+            if (userName != "")
+            {
+                Console.Write("Password: ");
+                password = Console.ReadLine();
+            }
+
+            if (userName == "system" && password == "manager")
+            {
+                int mainMenuChoice = -1;
+
+                do
+                {
+                    Console.WriteLine("\n:::Main menu:::");
+                    Console.WriteLine("1. Customers");
+                    Console.WriteLine("2. Accounts");
+                    Console.WriteLine("3. Funds Transfer");
+                    Console.WriteLine("4. Funds Transfer Statement");
+                    Console.WriteLine("5. Account Statement");
+                    Console.WriteLine("0. Exit");
+
+                    Console.Write("Enter choice: ");
+                    mainMenuChoice = int.Parse(Console.ReadLine());
+
+                    switch (mainMenuChoice)
+                    {
+                        case 1:
+                            CustomersMenu();
+                            break;
+                        case 2:
+                            AccountsMenu();
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            break;
+                    }
+                } while (mainMenuChoice != 0);
+            }
+            else
+            {
+                Console.WriteLine("Invalid username or password");
+            }
+
+            Console.WriteLine("Thank you! Visit again.");
+            Console.ReadKey();
+        }
+
+        static void CustomersMenu()
+        {
+            int customersMenuChoice = -1;
+
+            do
+            {
+                Console.WriteLine("\n:::Customers menu:::");
+                Console.WriteLine("1. Add Customer");
+                Console.WriteLine("2. Delete Customer");
+                Console.WriteLine("3. Update Customer");
+                Console.WriteLine("4. View Customer");
+                Console.WriteLine("0. Back to Main menu");
+
+                Console.Write("Enter choice: ");
+                customersMenuChoice = int.Parse(Console.ReadLine());
+            } while (customersMenuChoice != 0);
+        }
+
+        static void AccountsMenu()
+        {
+            int accountsMenuChoice = -1;
+
+            do
+            {
+                Console.WriteLine("\n:::Accounts menu:::");
+                Console.WriteLine("1. Add Account");
+                Console.WriteLine("2. Delete Account");
+                Console.WriteLine("3. Update Account");
+                Console.WriteLine("4. View Account");
+                Console.WriteLine("0. Back to Main menu");
+
+                Console.Write("Enter choice: ");
+                accountsMenuChoice = int.Parse(Console.ReadLine());
+            } while (accountsMenuChoice != 0);
+        }
+    }
+}
