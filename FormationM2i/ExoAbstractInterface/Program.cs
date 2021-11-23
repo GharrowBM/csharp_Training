7abdd101d9f1ca103bd4e7f21d4810dd0f19abfb
GharrowBM <gharrow.games@gmail.com>
2021-11-23 23:08:22 +0100

    exos SQL 23-11-21

--- FormationM2i/ExoAbstractInterface/Program.cs
+++ FormationM2i/ExoAbstractInterface/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using ExoAbstractInterface.Models;
 
 namespace ExoAbstractInterface
 {
@@ -6,7 +8,23 @@ namespace ExoAbstractInterface
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Figure> figures = new List<Figure>();
+
+            Square square = new Square(1, 5, 10);
+            Triangle triangle = new Triangle(4, 8, 12, 8);
+            Rectangle rectangle = new Rectangle(7, 8, 5, 10);
+
+            Figure test = square.Deform(2, 8);
+            Figure test2 = rectangle.Deform(1, 0.5);
+
+            figures.Add(square);
+            figures.Add(triangle);
+            figures.Add(rectangle);
+            figures.Add(test);
+            figures.Add(test2);
+
+            foreach(Figure f in figures) f.Print();
+
         }
     }
 }
