c40c050ce21b6566e553ee2fc3f3dc475a52792d
GharrowBM <gharrow.games@gmail.com>
2021-11-27 20:35:45 +0100

    End of FoodAI project

---
+++ FormationEduardoRosas/FoodAI/Classes/CustomVision.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodAI.Classes
+{
+    public class CustomVision
+    {
+        public string Id { get; set; }
+        public string Project { get; set; }
+        public string Iteration { get; set; }
+        public DateTime Created { get; set; }
+        public List<Prediction> Predictions { get; set; }
+
+    }
+
+}
