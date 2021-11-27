6e0115f4a3da2f6558bad0784f5979d15a92ada5
GharrowBM <gharrow.games@gmail.com>
2021-11-27 11:23:24 +0100

    Desktop Contacts App Done

---
+++ FormationEduardoRosas/DesktopContactsApp/ContactDetailsWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using DesktopContactsApp.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesktopContactsApp
+{
+    /// <summary>
+    /// Logique d'interaction pour ContactDetailsWindow.xaml
+    /// </summary>
+    public partial class ContactDetailsWindow : Window
+    {
+        Contact contact;
+
+        public ContactDetailsWindow(Contact contact)
+        {
+            InitializeComponent();
+
+            Owner = App.Current.MainWindow;
+            WindowStartupLocation = WindowStartupLocation.CenterOwner;
+
+            this.contact = contact;
+            emailTextBox.Text = contact.Email;
+            nameTextBox.Text = contact.Name;
+            phoneTextBox.Text = contact.Phone;
+
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            contact.Name = nameTextBox.Text;
+            contact.Phone = phoneTextBox.Text;
+            contact.Email = emailTextBox.Text;
+
+            using (SQLite.SQLiteConnection connection = new SQLite.SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                connection.Update(contact);
+            }
+
+            Close();
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (SQLite.SQLiteConnection connection = new SQLite.SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                connection.Delete(contact);
+            }
+
+            Close();
+        }
+    }
+}
