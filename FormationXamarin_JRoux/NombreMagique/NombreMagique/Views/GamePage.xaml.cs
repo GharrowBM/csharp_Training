e774686e1bf79a04e0d1b7fed8138978b7f53b29
GharrowBM <gharrow.games@gmail.com>
2022-01-13 20:51:58 +0100

    mystery number started

---
+++ FormationXamarin_JRoux/NombreMagique/NombreMagique/Views/GamePage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NombreMagique.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NombreMagique.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GamePage : ContentPage
+    {
+        public GameVM VM { get; set; }
+        
+        public GamePage()
+        {
+            InitializeComponent();
+
+            BindingContext = VM = new GameVM();
+        }
+    }
+}
